2241d757ee6263244b65b465f321745ec1d287c1
cpow24 <97103974+cpow24@users.noreply.github.com>
2022-09-12 01:07:44 +0100

    Add files via upload

---
+++ create_gene_set_files.R
@@ -0,0 +1,25 @@
+#Seting working directory
+setwd('ENTER DIRECTORY PATH')
+
+for (set in 1:64) {
+  
+  cmd_str <- paste0("plink --bfile target_train.QC --make-set Set_", as.character(set), ".txt --make-set-collapse-group --write-set --out snplist_set_", as.character(set))
+  system(paste (cmd_str ,sep=" "))
+  
+}
+
+#Processing set files
+for (set in 1:64){
+  set_list <- fread(paste0('snplist_set_',as.character(set),'.set'), header=TRUE)
+  
+  #Rename column
+  colnames(set_list) <- 'rsid'
+  
+  #Remove last 2 redundant rows
+  set_list <- head(set_list,-2)
+  
+  file_name <- paste0('snp_set_',as.character(set),'.txt')
+  write.table(set_list, file_name, col.names=F, quote=F, row.names=F)
+  
+}
+
